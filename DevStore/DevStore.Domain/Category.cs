698f5e781d79d520adaed84c0c2f0879e3d6b1ad
cleberspirlandeli <contato.cleberrezende@gmail.com>
2019-07-23 23:54:14 -0300

    remover .vs

---
+++ DevStore/DevStore.Domain/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace DevStore.Domain
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public override string ToString()
+        {
+            return this.Title;
+        }
+    }
+}
